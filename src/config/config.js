c84428d0cd308740d290d33630217f8b9dbd3a84
liuqi07 <18911690880@163.com>
2017-11-09 16:57:33 +0800

    '1'

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,8 @@ let config = {
     env: Env,
     // devBaseUrl: 'http://192.168.31.19:8080/api',
     // devBaseUrl: 'http://127.0.0.1:3000/api', // 接口服务启动在本机
-    // devBaseUrl: 'http://59.110.165.79:3000/api',
-    devBaseUrl: 'http://172.17.43.55:3000/api',
+    devBaseUrl: 'http://59.110.165.79:3000/api',
+    // devBaseUrl: 'http://172.17.43.55:3000/api',
+    webUrl: 'http://59.110.165.79:8080',
 };
 export default config;
